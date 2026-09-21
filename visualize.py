85810b37b6caa50896bd80b3bd569087ce82464d
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-09-21 18:15:35 +0300

    visualize.py updated (now second plot is lfo signal)

--- visualize.py
+++ visualize.py
@@ -7,7 +7,7 @@ from effects import tremolo
 
 data, fs = sf.read("clean-guitar-riff_E_minor.wav")
 
-rate, depth = 0.5, 0.5
+rate, depth = 0.3, 0.9
 
 output = tremolo(data, fs, rate=rate, depth=depth)
 
@@ -20,15 +20,23 @@ length = int(0.1 * fs)
 
 # Стороим сам график
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 1, 1)
 plt.plot(t[start:start+length], data[start:start+length, 0], label='Вход', alpha=0.7)
-plt.plot(t[start:start+length], output[start:start+length, 0], label="Выход", alpha=0.7)
-plt.plot(t[start:start+length], lfo[start:start+length], label='LFO', linestyle='--', color='red')
+plt.plot(t[start:start+length], output[start:start+length, 0], label='Выход', alpha=0.7)
 plt.xlabel('Время (с)')
 plt.ylabel('Амплитуда')
 plt.legend()
-plt.title('Тремоло: вход, выход, LFO (на отрезке 0.1 с)')
+plt.title('Вход и выход (тремоло)')
 plt.grid(True)
+
+plt.subplot(2, 1, 2)
+plt.plot(t[start:start+length], lfo[start:start+length], color='red', linestyle='--')
+plt.xlabel('Время (с)')
+plt.ylabel('LFO')
+plt.title('Управляющий сигнал LFO')
+plt.grid(True)
+
 plt.tight_layout()
-plt.savefig('time_domain.png')
 plt.show()
